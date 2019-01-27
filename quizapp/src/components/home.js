e5f4884bcc3f07e637f8c09a17549e5e88872536
John Otu <johnne01@gmail.com>
2019-01-27 17:50:04 +0000

    Added topic cards
    Arranged them in three columns

--- quizapp/src/components/home.js
+++ quizapp/src/components/home.js
@@ -3,11 +3,48 @@ import React from 'react';
 export default Home => {
   return (
     <div>
-      <div>
+      <div className='header'>
         <img src='https://i.postimg.cc/3rBgrrNY/Dev-Quiz-2.png' width='150px' alt='DevC Accra Logo' />
       </div>
       <div className='content'>
-        <h3>Home</h3>
+        <h3>Choose a topic to test your knowledge</h3>
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              <p>icon</p>
+              <h3>Linux Commands</h3>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <p>icon</p>
+              <h3>Git</h3>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <p>icon</p>
+              <h3>HTML</h3>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              <p>icon</p>
+              <h3>CSS</h3>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <p>icon</p>
+              <h3>JavaScript</h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
