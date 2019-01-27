ed2d865b8148ebf9fc1f232a15548ec005af5d40
John Otu <johnne01@gmail.com>
2019-01-27 07:45:52 +0000

    Added image assets
    Favicon and logo

--- quizapp/src/components/home.js
+++ quizapp/src/components/home.js
@@ -3,7 +3,12 @@ import React from 'react';
 export default Home => {
   return (
     <div>
-      <h3>Home</h3>
+      <div>
+        <img src='https://i.postimg.cc/3rBgrrNY/Dev-Quiz-2.png' width='150px' alt='DevC Accra Logo' />
+      </div>
+      <div className='content'>
+        <h3>Home</h3>
+      </div>
     </div>
   )
 }
