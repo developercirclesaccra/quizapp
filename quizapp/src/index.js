72dae3dbb6fe0c6540d5f2ab199558a713e77da2
John Otu <johnne01@gmail.com>
2019-03-05 12:12:52 +0000

    moved component files
    ...to src folder for easy access

--- quizapp/src/index.js
+++ quizapp/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css'
 import './index.css';
 
-import Home from './components/home';
+import Home from './home';
 
 export default class DevQuiz extends React.Component {
   render() {
