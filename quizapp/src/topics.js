6af03c345b2ecb837ce5a5e3f231b8c93ef6858d
John Otu <johnne01@gmail.com>
2019-03-06 01:12:05 +0000

    added topics file
    ...temporarily to allow successful build

---
+++ quizapp/src/topics.js
@@ -0,0 +1,40 @@
+const topics = [
+  {
+    id: 1,
+    header: "Linux Commands",
+    image: "https://i.postimg.cc/ZR8CP8KL/logo-terminal.png",
+    description: "How much Linux power do you wield?"
+  },
+  {
+    id: 2,
+    header: "Git",
+    image: "https://i.postimg.cc/DwNJ7fM1/logo-git.png",
+    description: "Test yourself on the most popular version control system"
+  },
+  {
+    id: 3,
+    header: "HTML",
+    image: "https://i.postimg.cc/cHyv89ZH/logo-html.png",
+    description: "Do you know the standard markup language for the web?"
+  },
+  {
+    id: 4,
+    header: "CSS",
+    image: "https://i.postimg.cc/fb03y4Gp/logo-css.png",
+    description: "How's your style?"
+  },
+  {
+    id: 5,
+    header: "JavaScript",
+    image: "https://i.postimg.cc/8zcf8ft1/logo-javascript.png",
+    description: "Show off the programming language of HTML and the Web"
+  },
+  {
+    id: 6,
+    header: "Python",
+    image: "https://i.postimg.cc/sx31TMMC/logo-python.png",
+    description: "You think you know some Python?"
+  }
+];
+
+export default topics;
